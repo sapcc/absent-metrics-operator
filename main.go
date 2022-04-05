1aee54b379eb84fd1b0a625d6b3ad16e9e090723
Muhammad Talal Anwar <muhammad.talal.anwar@sap.com>
2022-04-05 18:52:55 +0200

    Add `monitoringv1` to scheme

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -50,6 +51,7 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(monitoringv1.AddToScheme(scheme))
 
 	//+kubebuilder:scaffold:scheme
 }
@@ -85,7 +87,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "62fc53df.cloud.sap",
+		LeaderElectionID:       "absent-metrics-operator.cloud.sap",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
